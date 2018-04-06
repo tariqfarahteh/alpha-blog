31a61ba05c7d8dcfd46dde3bfdde51da73a195f6
tariqfarahteh <tariqfarahteh@gmail.com>
2018-04-06 14:34:08 -0400

    Compleate authontication system

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ root 'pages#home'
   get 'signup', to: 'users#new'
   
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
